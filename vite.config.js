49d115fa5f96fbeb74f3ed102184437ab60650a8
Emanuel Lugo <lugo.emanuel@gmail.com>
2025-03-26 22:19:59 -0400

    added subpath for gh pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
+  base: "/web-audio-app/", // 👈 This is the key part for GitHub Pages
   plugins: [
     react(),
     tailwindcss(),
